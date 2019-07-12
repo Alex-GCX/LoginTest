91b32e17bf4c7681b83db810acf921995fd5aff7
gongchengxue <g1242556827@163.com>
2019-07-12 08:21:13 +0800

    添加注释

--- GithubLogin.py
+++ GithubLogin.py
@@ -25,16 +25,21 @@ def get_login_cookies(url, token, cookies):
             'login': 'gongchengxue',
             'password': '962464huang612',
             'webauthn-support': 'supported'}
-    response = requests.post(url, data=data, cookies=cookies)
-    print(response.status_code)
+    response = requests.post(url, data=data, cookies=cookies)#222 #333
+    print(response.status_code, response.reason)
     cookies = response.cookies.get_dict()
     return cookies
-
 
 if __name__ == '__main__':
     html, cookies = get_github_html(base_url)
     token = get_token(html)
     login_cookies = get_login_cookies(login_url, token, cookies)
     print(login_cookies)
-    response = requests.get('https://github.com/settings/repositories',cookies=login_cookies)
-    print(response.status_code,response.reason)
+    response = requests.get('https://github.com/settings/repositories', cookies=login_cookies)#1111
+    print(response.status_code, response.reason)
+'''
+1.最终程序能够访问到 'https://github.com/settings/repositories' ,重点就是如何得到请求中的 login_cookies
+2.如何拿到 login_cookies,就是通过模拟登陆,再拿到登录后response中的cookies
+3.如何模拟登陆,就是先手动登录,然后通过看登录form表单中通过POST请求的登录Form Data(重点是其中的 authenticity_token),
+  拿到data,cookies就是登录之前登录页面的response的cookies
+'''
